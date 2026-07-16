a33b5cf31fd70bf8d9e866e034a8d49a3f239d8b
Tanush Varayogi <tanushchandhra.vyogi@gmail.com>
2026-07-16 23:33:25 +0530

    Time: 0 ms (100%), Space: 12.5 MB (20.18%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        dp =[0]*(n+1)
+        dp[0]=1
+        for i in range(1,n+1):
+            if i>=1:
+                dp[i]+=dp[i-1]
+            if i>=2:
+                dp[i]+=dp[i-2]
+        return dp[n]
+            
+       
+       
+      
