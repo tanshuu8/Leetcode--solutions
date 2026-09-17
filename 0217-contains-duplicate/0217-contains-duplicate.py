6709f0062eb0674cafa22ce60d7f4baec86ab6a1
Tanush Varayogi <tanushchandhra.vyogi@gmail.com>
2026-09-17 21:10:28 +0530

    Time: 60 ms (11.79%), Space: 28.5 MB (11.97%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        freq ={}
+        for num in nums:
+            if num in freq:
+                freq[num]+=1
+            else:
+                freq[num]=1
+        for key in freq:
+            if freq[key]>1:
+                return True
+        return False
+
+
+                
