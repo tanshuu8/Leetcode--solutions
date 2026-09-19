f2bd0aff5c9783c6d4bd4f4f86ebfff137ba3115
Tanush Varayogi <tanushchandhra.vyogi@gmail.com>
2026-09-19 22:46:01 +0530

    Time: 268 ms (5.84%), Space: 13 MB (46.32%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        new =""
+        for ch in s:
+            if ch.isalnum():
+                new+=ch.lower()
+        return new == new[::-1]
+       
