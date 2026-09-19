a342fea226d2b734310a4fe70e60503ee7d24cee
Tanush Varayogi <tanushchandhra.vyogi@gmail.com>
2026-09-19 23:37:51 +0530

    Time: 7 ms (13.44%), Space: 12.6 MB (11.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,18 +2,22 @@ class Solution(object):
     def isValid(self, s):
         stack =[]
         for ch in s:
-            if ch == '(' or ch=='[' or ch == '{':
+            if ch == '(' or ch =='[' or ch=='{':
                 stack.append(ch)
-            elif ch==')':
+            elif ch == ')':
                 if not stack or stack.pop()!='(':
                     return False
-            elif ch ==']':
+            elif ch == ']':
                 if not stack or stack.pop()!='[':
                     return False
-            elif ch =='}':
+            elif ch == '}':
                 if not stack or stack.pop()!='{':
                     return False
         return len(stack)==0
+
+    
+       
+        
             
 
         
