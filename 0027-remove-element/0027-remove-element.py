ecc9b77de2d478be14de65c4c8b98947087f9899
Tanush Varayogi <tanushchandhra.vyogi@gmail.com>
2026-09-20 16:05:28 +0530

    Time: 0 ms (100%), Space: 12.3 MB (55.51%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,11 +1,14 @@
 class Solution(object):
     def removeElement(self, nums, val):
-        slow =0
+        slow=0
         for fast in range(len(nums)):
             if nums[fast]!=val:
                 nums[slow]=nums[fast]
                 slow+=1
         return slow
+
+       
+        
         
        
         
