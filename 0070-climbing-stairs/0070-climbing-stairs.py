51b083349ce9a372955f3953bfae6da74ba0b199
Tanush Varayogi <tanushchandhra.vyogi@gmail.com>
2026-09-21 21:35:36 +0530

    Time: 0 ms (100%), Space: 12.4 MB (20.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,7 +4,8 @@ class Solution(object):
         dp[0]=1
         for i in range(1,n+1):
             if i>=1:
-                dp[i]+=dp[i-1]
+                dp[i]+= dp[i-1]
             if i>=2:
                 dp[i]+=dp[i-2]
         return dp[n]
+       
